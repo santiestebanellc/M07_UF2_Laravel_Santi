25af23a0358c88c56c78ae702392c50645f5ec3f
santiestebanellc <santiestebanellc@gmail.com>
2025-02-09 19:49:40 +0100

    country and duration

--- resources/views/films/list.blade.php
+++ resources/views/films/list.blade.php
@@ -6,12 +6,12 @@
     <div align="center">
     <table border="1">
         <tr>
-            @foreach($films as $film)
-                @foreach(array_keys($film) as $key)
-                    <th>{{$key}}</th>
-                @endforeach
-                @break
-            @endforeach
+            <th>Nombre</th>
+            <th>Año</th>
+            <th>Género</th>
+            <th>País</th>
+            <th>Duración</th>
+            <th>Imagen</th>
         </tr>
 
         @foreach($films as $film)
@@ -19,7 +19,9 @@
                 <td>{{$film['name']}}</td>
                 <td>{{$film['year']}}</td>
                 <td>{{$film['genre']}}</td>
-                <td><img src={{$film['img_url']}} style="width: 100px; height: 120px;" /></td>
+                <td>{{$film['country']}}</td>
+                <td>{{$film['duration']}} min</td>
+                <td><img src="{{$film['img_url']}}" style="width: 100px; height: 120px;" /></td>
             </tr>
         @endforeach
     </table>
